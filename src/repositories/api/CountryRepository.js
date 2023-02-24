364557730ad96981edb43a3789cf1a2031cf5499
Martí Puig Martin <104025496+martipuigmartin@users.noreply.github.com>
2023-02-24 13:43:17 +0100

    Update CountryRepository.js

--- src/repositories/api/CountryRepository.js
+++ src/repositories/api/CountryRepository.js
@@ -1,5 +1,10 @@
+import {useQuery} from '@tanstack/react-query'
+
 import Repository from '../BaseRepository'
 
-export default {
-  list: () => Repository.get(''),
+const fetchCountryList = async () => {
+  const {data} = await Repository.get('')
+  return data
 }
+
+export const useCountryList = () => useQuery(['countryList'], fetchCountryList)
