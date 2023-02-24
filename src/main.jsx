481dc7d79c8942a1dc89834136cecf470d35902b
Martí Puig Martin <104025496+martipuigmartin@users.noreply.github.com>
2023-02-24 13:44:19 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import {App} from './App'
 
+const client = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
